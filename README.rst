1d72405aaffed8f797fd5a96ea2b6bc39b28ab15
user <d.vardyzer@gmail.com>
2017-01-08 05:38:01 -0800

    adding icon to README

--- README.rst
+++ README.rst
@@ -2,4 +2,4 @@ FOOBAR
 ======
 
 This is an example project
-
+[! [Build Status](https://travis-ci.org/daniellevz/foobar.svg?branch=master)](https://travis-ci.org/daniellevz/foobar)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,2 @@
 import os, sys
-#sys.path.append(os.path.abspath('..')) # module foobar has no attribute foo
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
